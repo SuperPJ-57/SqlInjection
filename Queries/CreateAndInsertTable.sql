3cf0ae7cab0d8ee8635fa91de8c6dddd63352523
Prajjwal Gwachha <prajjwaljohnny@gmail.com>
2024-11-28 16:39:58 +0545

    added queries

---
+++ Queries/CreateAndInsertTable.sql
@@ -0,0 +1,13 @@
+﻿CREATE DATABASE SQLInjectionDemo;
+
+USE SQLInjectionDemo;
+
+CREATE TABLE Users (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    Username NVARCHAR(50) NOT NULL,
+    Password NVARCHAR(50) NOT NULL
+);
+
+INSERT INTO Users (Username, Password)
+VALUES ('admin', 'password123'),
+       ('testuser', 'test123');
